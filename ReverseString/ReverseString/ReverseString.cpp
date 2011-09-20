c4bbc07814e424e3177c38b6efc15e94763fb905
tenparsec <tenparsec@ya.ru>
2011-09-20 23:15:38 +0300

    new best solution ReverseString.cpp

--- ReverseString/ReverseString/ReverseString.cpp
+++ ReverseString/ReverseString/ReverseString.cpp
@@ -1,30 +1,31 @@
-// перевернуть строку не использу€ дополнительный буфер!!!
-// вообще € думал над задачей и скорее всего имелось ввиду
-// не использовать именно временную переменную дл€ обмена,
-// потому он и решал побитово...
+// перевернуть строку не использу€ дополнительный Ѕ”‘≈–!!!
+// как € пон€л нельз€ использовать ни временную переменную, ни дополнительную линию
 
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
 
+void reverse(char *String);
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	char string[] = "This line should be rotated";
-	int len = strlen(string);
+	char String[] = "This line should be rotated";
 
-	cout << "Befor: " << string << endl;
-	for(int i = 0; i < len; ++i)
-	{
-		if(i == len || i > len - i - 1)
-			break;
-		string[ len ] = string[ i ];
-		string[ i ] = string[ len - i - 1 ];
-		string[ len - i - 1 ] = string[ len ];
-	}
-	string[ len ] = 0;
-
-	cout << "After: " << string << endl;
+	cout << "Befor: " << String << endl;
+	reverse(String);
+	cout << "After: " << String << endl;
 
 	return 0;
 }
 
+void reverse(char *String)
+{
+	int len = strlen(String) - 1;
+
+	for(int i = 0; i < len; ++i, --len)
+	{
+		String[ len ] ^= String[ i ];
+		String[ i ] ^= String[ len ];
+		String[ len ] ^= String[ i ];
+	}
+}
